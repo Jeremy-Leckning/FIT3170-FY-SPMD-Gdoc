3418d97e066a28590c81cc7b863cffa6ae671144
fongcl <cfon0005@student.monash.edu>
2020-09-26 13:03:55 +1000

    Added Searchbar without functionality, auto zoom to current location

--- app/components/GooglePlacesInput.js
+++ app/components/GooglePlacesInput.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
- 
+import React from "react";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+
 const GooglePlacesInput = () => {
   return (
-    
     <GooglePlacesAutocomplete
-      placeholder='Search'
+      placeholder="Search place"
       onPress={(data, details = null) => {
-        console.log("test")
+        console.log("test");
         // 'details' is provided when fetchDetails = true
         console.log(data, details);
       }}
       query={{
-        key: '',
-        language: 'en',
-        components: 'country:au',
+        key: "",
+        language: "en",
       }}
-      currentLocation={true}
-      currentLocationLabel='Current location'
-      
     />
   );
 };
- 
+
 export default GooglePlacesInput;
--- app/screens/ParkScreen.js
+++ app/screens/ParkScreen.js
@@ -1,48 +1,116 @@
-import React, { Component } from 'react';
-import { ImageBackground, Text, StyleSheet, View } from 'react-native';
-import { SearchBar } from 'react-native-elements';
-import { SafeAreaView } from 'react-navigation';
-import MapView from 'react-native-maps';
+import React, { Component } from "react";
+import { ImageBackground, Text, StyleSheet, View, ScrollView, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { SearchBar } from "react-native-elements";
+import { SafeAreaView } from "react-navigation";
+import MapView, { AnimatedRegion } from "react-native-maps";
+import GooglePlacesInput from "../components/GooglePlacesInput";
 
-class ParkScreen extends Component{
+class ParkScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      search: '',
-    }
+      search: "",
+      initialRegion: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      loading: true,
+    };
   }
 
   updateSearch = (search) => {
     this.setState({ search });
   };
-  
-  render(){
+
+  success = (pos) => {
+    this.setState(
+      {
+        initialRegion: {
+          latitude: parseFloat(pos.coords.latitude),
+          longitude: parseFloat(pos.coords.longitude),
+          latitudeDelta: 5,
+          longitudeDelta: 5,
+        },
+      },
+      () => {
+        this.setState({ loading: false });
+      }
+    );
+  };
+
+  error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
+  getCurrentLocation() {
+    navigator.geolocation.getCurrentPosition(this.success, this.error, {
+      maximumAge: 60000,
+      timeout: 5000,
+      enableHighAccuracy: true,
+    });
+  }
+
+  goToInitialLocation() {
+    let initialRegion = Object.assign({}, this.state.initialRegion);
+    initialRegion["latitudeDelta"] = 0.005;
+    initialRegion["longitudeDelta"] = 0.005;
+    this.mapView.animateToRegion(initialRegion, 2000);
+  }
+
+  componentDidMount() {
+    this.getCurrentLocation();
+  }
+
+  render() {
     const { search } = this.state;
-    return (
-      <View style={styles.container}>
-          <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}/>
-      </View>
-    )
+    if (!this.state.loading) {
+      return (
+        <View style={styles.container}>
+        <MapView
+            style={styles.map}
+            region={this.state.mapRegion}
+            followUserLocation={true}
+            ref={(ref) => (this.mapView = ref)}
+            zoomEnabled={true}
+            showsUserLocation={true}
+            onMapReady={this.goToInitialLocation.bind(this)}
+            initialRegion={this.state.initialRegion}
+          />          
+          <View style={{ width: '100%', position: "absolute", top: 50 }}>
+            <SearchBar
+              platform="ios"
+              containerStyle={{backgroundColor: 'transparent'}}
+              inputContainerStyle={{backgroundColor: 'white', borderColor: 'gray', borderRadius: 10}}
+              inputStyle={{backgroundColor: 'white'}}
+              leftIconContainerStyle={{backgroundColor: 'white'}}
+              placeholder="Search"
+              onChangeText={this.updateSearch}
+              value={search}
+              lightTheme
+              showCancel
+              cancelButtonTitle="Cancel"
+              round
+            />
+          </View>
+        </View>
+      );
+    } else {
+      return <Text>Loading...</Text>;
+    }
   }
 }
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "white",
-      // alignItems: "center",
-      // justifyContent: "flex-start",
-    },
-    map: {
-      ...StyleSheet.absoluteFillObject,
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    // alignItems: "center",
+    // justifyContent: "flex-start",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default ParkScreen;
